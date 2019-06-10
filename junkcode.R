60f8b4e053890a8fd791e8ff0b1370f1b85316d8
Nicholas Oxford <nicholasoxford@onepeloton.com>
2019-06-09 20:29:36 -0400

    uploading code sandboxes that I expirment in

--- junkcode.R
+++ junkcode.R
@@ -5,34 +5,26 @@ data(fips_codes)
 ##remove columns from fips_code that do not relate to georiga.
 fips_codes <- fips_codes[-(548:3237), ]
 fips_codes <- fips_codes[-(1:388), ]
-sampledf <- daily_fips(fips = 13001, date_min = "2019-01-01", 
-                       date_max = "2019-01-01", var = "tmax")
+sampledf <- daily_fips(fips = 13001, date_min = date, 
+                       date_max = date, var = "tmax")
 ##using this to create a dataframe that I can add to later
 ##Delete varibales I don't need 
-sampledf$station_map <- NULL
-sampledf$station_metadata <- NULL
-sampledf$daily_data$tmax_reporting <- NULL
 sampledf$daily_data$fips <- 13001
 new.df <- rbind(sampledf$daily_data)
-rm(sampledf)
 #makenew df that I will add to later
 gafips <- 13000
 countyfips <- 15
 ##Going to have this while loop eventually run through all GA FIPS codes. 
 ##I was thinking a for loop could be better
-while ( countyfips <= 337 ){
-  options("noaakey" = Sys.getenv("noaakey"))
+while ( countyfips <= 30 ){
   fipsnum= gafips + countyfips
   countytest <- as.character(countyfips)
   countytest1 <- paste("0", countytest, sep="")
   ##running an if statement to check if the fips code is one used in georiga.
   ##if true it grabs the data and does the same manipulation as above
   if(countytest1 %in% fips_codes$county_code == TRUE){
-    tempdata <- daily_fips(fips = 13019, date_min = "2019-01-01", 
-                           date_max = "2019-01-01", var = "tmax")
-    tempdata$station_map <- NULL
-    tempdata$station_metadata <- NULL
-    tempdata$daily_data$tmax_reporting <- NULL
+    tempdata <- daily_fips(fips = fipsnum, date_min = date, 
+                           date_max = "2019-02-01", var = "tmax")
     tempdata$daily_data$fips <- fipsnum
     new.df <- rbind( tempdata$daily_data,new.df)
     rm(tempdata)
---
+++ rnoaaTest.R
@@ -0,0 +1,5 @@
+dataframe <- ncdc_locs(locationcategoryid='CITY', sortfield='name', sortorder='desc')
+shp <- tornadoes()
+stations <- isd_stations()
+ncdc_datacats(locationid = 'FIPS:13017')
+stations <- isd_stations()
