255abb0af9f2e17d55086c66c60c49701bde5541
Nicholas Oxford <nicholasoxford@onepeloton.com>
2019-06-09 23:15:05 -0400

    small changes

--- dataframeDataScript.R
+++ dataframeDataScript.R
@@ -1,42 +1,38 @@
-##Set Date for countrykey, define plus.1 as a variable and load fips_code
+
 date <- "2019-01-01"
 plus.1 <- 1
 data(fips_codes)
-##remove columns from fips_code that do not relate to georiga.
+talk <- "blah blah"
+print(talk)
 fips_codes <- fips_codes[-(548:3237), ]
 fips_codes <- fips_codes[-(1:388), ]
-sampledf <- daily_fips(fips = 13001, date_min = "2019-01-01", 
+sampledf <- daily_fips(fips = "13003", date_min = "2019-01-01", 
                        date_max = "2019-01-01", var = "tmax")
-##using this to create a dataframe that I can add to later
-##Delete varibales I don't need 
 sampledf$station_map <- NULL
 sampledf$station_metadata <- NULL
 sampledf$daily_data$tmax_reporting <- NULL
 sampledf$daily_data$fips <- 13001
 new.df <- rbind(sampledf$daily_data)
 rm(sampledf)
-#makenew df that I will add to later
+
 gafips <- 13000
 countyfips <- 15
-##Going to have this while loop eventually run through all GA FIPS codes. 
-##I was thinking a for loop could be better
+
 while ( countyfips <= 337 ){
-  options("noaakey" = Sys.getenv("noaakey"))
   fipsnum= gafips + countyfips
   countytest <- as.character(countyfips)
   countytest1 <- paste("0", countytest, sep="")
-  ##running an if statement to check if the fips code is one used in georiga.
-  ##if true it grabs the data and does the same manipulation as above
   if(countytest1 %in% fips_codes$county_code == TRUE){
-    tempdata <- daily_fips(fips = 13019, date_min = "2019-01-01", 
+    tempdata <- daily_fips(fips = "13019", date_min = "2019-01-01", 
                            date_max = "2019-01-01", var = "tmax")
+    andrew_precip <- daily_fips(fips = "12086", date_min = "1992-08-01", 
+                                date_max = "1992-08-31", var = "prcp")
     tempdata$station_map <- NULL
     tempdata$station_metadata <- NULL
     tempdata$daily_data$tmax_reporting <- NULL
     tempdata$daily_data$fips <- fipsnum
     new.df <- rbind( tempdata$daily_data,new.df)
     rm(tempdata)
-    ##I thought removing the tempdata would solve my column id issue
   } else { print (paste("Nothing found for", fipsnum, sep=" "))
     
   }
