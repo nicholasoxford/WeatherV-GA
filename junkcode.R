7a5ec433aae03176a5af72a3f9e67dbb2ff4f0da
Nicholas Oxford <nicholasoxford@onepeloton.com>
2019-06-10 23:20:21 -0400

    updating random junk code

--- junkcode.R
+++ junkcode.R
@@ -1,4 +1,5 @@
 ##expirments I run
+
 ##Set Date for countrykey, define plus.1 as a variable and load fips_code
 date <- "2019-01-01"
 plus.1 <- 1
@@ -36,3 +37,18 @@ while ( countyfips <= 30 ){
   countyfips <- plus.1 + countyfips
 }
 
+
+## remove all packages
+ip <- as.data.frame(installed.packages())
+head(ip)
+# if you use MRO, make sure that no packages in this library will be removed
+ip <- subset(ip, !grepl("MRO", ip$LibPath))
+# we don't want to remove base or recommended packages either\
+ip <- ip[!(ip[,"Priority"] %in% c("base", "recommended")),]
+# determine the library where the packages are installed
+path.lib <- unique(ip$LibPath)
+# create a vector with all the names of the packages you want to remove
+pkgs.to.remove <- ip[,1]
+head(pkgs.to.remove)
+# remove the packages
+sapply(pkgs.to.remove, remove.packages, lib = path.lib)
